b4caab07b96ebc8c920a72de6486674301cf415f
Saiffer <32097584+Saiffer@users.noreply.github.com>
2020-02-27 22:06:01 -0500

    asd

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src="https://i1.pngguru.com/preview/955/183/727/metronome-blue-and-white-headphones-icon-logo-png-clipart-thumbnail.jpg" />
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Profile = () => {
+    return (
+        <div className='content'>
+        <div>
+          <img src='https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png' />
+        </div>
+        <div>ava+description</div>
+        <div>
+          My posts
+          <div>New post</div>
+          <div>
+            <div>1</div>
+            <div>2</div>
+          </div>
+        </div>
+      </div>
+    );
+}
+
+export default Profile;
